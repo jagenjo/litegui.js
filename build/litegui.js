5e75fae33b4dd9b06e6fa11389ee997056858b02
Frank de Weger <frankdeweger@gmail.com>
2015-08-07 13:27:14 +0200

    Don't commit build/, let's fix src/ instead :)

--- build/litegui.js
+++ build/litegui.js
@@ -3400,7 +3400,6 @@ function beautifyCode(code, reserved)
 	*/
 	Tree.prototype.insertItem = function(data, parent_id, position, options)
 	{
-        options = options || {};
 		if(!parent_id)
 		{
 			var root = this.root.childNodes[0];
